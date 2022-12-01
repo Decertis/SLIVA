868d738be8884f442ec45848a2031ff28e0a52e1
Decertis <maxx.greeb@gmail.com>
2022-12-01 14:47:48 +0100

    MySqlManager and User classes

---
+++ SLIVA/Data/MySqlManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using SLIVA.Models;
+namespace SLIVA.Data
+{
+    public static class MySqlManager
+    {
+        static MySqlConnection connection = new MySqlConnection("Server=127.0.0.1;UserId=root;Password=55665566;Database=SLIVA_db;");
+        public static User GetUser(UserAuthenticationData auth_data)
+        {
+            connection.Open();
+            var command = new MySqlCommand($"SELECT * FROM Users WHERE user_login = '{auth_data.Login}';",connection);
+            var reader = command.ExecuteReader();
+            reader.Read();
+            if (!reader.HasRows)
+            {
+
+                reader.Close();
+                connection.Close();
+                throw new AggregateException("User does not exist!");
+            }
+
+            string user_password = reader.GetValue(1).ToString();
+            if (auth_data.Password == user_password)
+            {
+                string user_name = reader.GetValue(0).ToString();
+                string user_email = reader.GetValue(2).ToString();
+                int user_id = reader.GetInt32(3);
+                string user_login = reader.GetValue(4).ToString();
+
+                reader.Close();
+                connection.Close();
+
+                return new User(user_name, user_login, user_password, user_email, user_id);
+            }
+
+            reader.Close();
+            connection.Close();
+            return null;
+
+        }
+        public static bool UserExists(string login)
+        {
+            connection.Open();
+            var command = new MySqlCommand($"SELECT * FROM Users WHERE user_login = '{login}';",connection);
+
+            var reader = command.ExecuteReader();
+            reader.Read();
+
+            if (reader.HasRows)
+            {
+                connection.Close();
+                reader.Close();
+                return true;
+            }
+            connection.Close();
+            reader.Close();
+
+            return false;
+        }
+
+    }
+}
+
---
+++ SLIVA/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json;
+using SLIVA.Data;
+namespace SLIVA.Models
+{
+    public class UserAuthenticationData
+    {
+        /* Serialization/Deserialization of json file
+            var authenticationData = new UserAuthenticationData()
+            {
+                Login = "decertis",
+                Password = "55665566"
+            };
+
+            string json_string = JsonConvert.SerializeObject(authenticationData);
+            Console.WriteLine(json_string);
+
+            authenticationData = JsonConvert.DeserializeObject<UserAuthenticationData>(json_string);
+            Console.WriteLine($"Login {authenticationData.Login}\nPassword {authenticationData.Password}");
+            */
+
+        public string Login;
+        public string Password;
+    }
+    public class User
+    {
+        public string Username;
+        public string Login;
+        public string Password;
+        public string Emali;
+        public int Id;
+
+        public User(string username, string login, string password, string email, int id)
+        {
+            Username = username;
+            Login = login;
+            Password = password;
+            Emali = email;
+            Id = id;
+        }
+    }
+}
